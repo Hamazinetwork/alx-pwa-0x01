74dca737925f827b7a23d82481e84fa1ff7af939
hamazinetwork <hademolahamazin@gmail.com>
2025-07-01 16:25:57 +0100

    first commit

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit(
+  {
+    dest: 'public'
+  }
+)
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'm.media-amazon.com',
+    ]
+  },
 };
-
-export default nextConfig;
+export default withPWA({
+  ...nextConfig
+})
